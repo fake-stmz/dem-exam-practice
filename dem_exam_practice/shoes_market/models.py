ab6b80536f219d33d9249d65efdc5a8688fe6619
student-pro <student-pro@b301-2.khpk.ru>
2026-01-16 14:15:03 +0700

    Created models & made migrations

--- dem_exam_practice/shoes_market/models.py
+++ dem_exam_practice/shoes_market/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class MeasureUnit(models.Model):
+    name = models.CharField()
+
+
+class Supplier(models.Model):
+    name = models.CharField()
+
+
+class Producer(models.Model):
+    name = models.CharField()
+
+
+class Category(models.Model):
+    name = models.CharField()
+
+
+class Status(models.Model):
+    name = models.CharField()
+
+
+class PickupPoint(models.Model):
+    postcode = models.IntegerField()
+    city = models.CharField()
+    street = models.CharField()
+    house = models.IntegerField()
+
+
+class Product(models.Model):
+    article = models.CharField(primary_key=True)
+    name = models.CharField()
+    measure_unit = models.ForeignKey(MeasureUnit, on_delete=models.CASCADE, related_name="product")
+    price = models.IntegerField()
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="product")
+    producer = models.ForeignKey(Producer, on_delete=models.CASCADE, related_name="product")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product")
+    discount = models.IntegerField()
+    quantity = models.IntegerField()
+    description = models.TextField()
+    picture = models.CharField()
