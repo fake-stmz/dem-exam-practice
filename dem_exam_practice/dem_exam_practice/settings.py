c3901eb84da00dbdaa9422e85fa79eb37cb63eb6
student-pro <student-pro@b301-2.khpk.ru>
2026-01-16 13:55:50 +0700

    Configured DB connection

--- dem_exam_practice/dem_exam_practice/settings.py
+++ dem_exam_practice/dem_exam_practice/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dem_exam_practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dedb',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': ''
     }
 }
 
